9b1b9d55a8dfa3b73c720b9126e60316cba2fd44
walt <walt>
2015-09-02 01:20:15 +0200

    removed dependency of bash 4, for now

--- fzf_browser.sh
+++ fzf_browser.sh
@@ -43,34 +43,34 @@ fuzzyfile() {
 
 
 
-declare -A __type_ext_map=( \
-  ["audio"]="au mid midi mka mpc ra axa oga spx xspf flac ogg mp3 m4a aac wav" \
-  ["video"]="avi mov m2v ogm mp4v vob qt nuv asd rm rmvb flc fli gl m2ts divx axv anx ogv ogx mkv webm flv mp4 m4v mpg mpeg" \
-  ["image"]="gif bmp pbm pgm ppm tga xbm xpm tif tiff svg svgz mng pcx dl xcf xwd yuv cgm emf eps cr2 ico jpg jpeg png" \
-  ["binary"]="msi exe fla" \
-  ["archives"]="iso xz zip tar 7z gz bz bz2 apk tgz lzma arj taz lzh tlz txz z dz lz tbz tbz2 tz deb rpm jar ace rar zoo cpio rz gem" \
-  ["document"]="docx pdf odt" \
-  ["text"]="nfo markdown asciidoc cfg conf" \
-  ["markup"]="xml html tex haml css" \
-  ["code"]="ada cs d nim c cpp hs rb lua moon fs go py sh js sql tex" \
-  ["dbase"]="sqlite" \
-  ["junk"]="log bak aux lof lol lot toc bbl blg tmp temp swp incomplete o class cache pyc aria2 torrent torrent.added part crdownload" \
-  ["CodeMarkup"]="|code markup|" \
-  ["Text"]="|text CodeMarkup|" \
-)
+#declare -A __type_ext_map=( \
+  #["audio"]="au mid midi mka mpc ra axa oga spx xspf flac ogg mp3 m4a aac wav" \
+  #["video"]="avi mov m2v ogm mp4v vob qt nuv asd rm rmvb flc fli gl m2ts divx axv anx ogv ogx mkv webm flv mp4 m4v mpg mpeg" \
+  #["image"]="gif bmp pbm pgm ppm tga xbm xpm tif tiff svg svgz mng pcx dl xcf xwd yuv cgm emf eps cr2 ico jpg jpeg png" \
+  #["binary"]="msi exe fla" \
+  #["archives"]="iso xz zip tar 7z gz bz bz2 apk tgz lzma arj taz lzh tlz txz z dz lz tbz tbz2 tz deb rpm jar ace rar zoo cpio rz gem" \
+  #["document"]="docx pdf odt" \
+  #["text"]="nfo markdown asciidoc cfg conf" \
+  #["markup"]="xml html tex haml css" \
+  #["code"]="ada cs d nim c cpp hs rb lua moon fs go py sh js sql tex" \
+  #["dbase"]="sqlite" \
+  #["junk"]="log bak aux lof lol lot toc bbl blg tmp temp swp incomplete o class cache pyc aria2 torrent torrent.added part crdownload" \
+  #["CodeMarkup"]="|code markup|" \
+  #["Text"]="|text CodeMarkup|" \
+#)
 
-declare -A __type_name_map=( \
-  ["code"]="Makefile Rakefile" \
-)
+#declare -A __type_name_map=( \
+  #["code"]="Makefile Rakefile" \
+#)
 
 function __join { local IFS="$1"; shift; echo "$*"; }
 
 typext(){
-  local ret=()
-  for k in "$@"; do
-    ret+=($(__join \| "${__type_ext_map["$k"]}"))
-  done
-  local ret2=$(__join \| "${ret[@]}")
+  #local ret=()
+  #for k in "$@"; do
+    #ret+=($(__join \| "${__type_ext_map["$k"]}"))
+  #done
+  local ret2=$(__join \| "xml html tex haml css nfo markdown asciidoc cfg conf ada cs d nim c cpp hs rb lua moon fs go py sh js sql tex")
   printf "%q$" "($ret2)"
 }
 
