448777f7170f67d530dcaab9c9f1a255f4be4fb8
walt <walt>
2015-09-02 01:31:11 +0200

    fixxxx

--- fzf_browser.sh
+++ fzf_browser.sh
@@ -66,11 +66,12 @@ fuzzyfile() {
 function __join { local IFS="$1"; shift; echo "$*"; }
 
 typext(){
-  #local ret=()
+  local ret=()
   #for k in "$@"; do
     #ret+=($(__join \| "${__type_ext_map["$k"]}"))
   #done
-  local ret2=$(__join \| "xml html tex haml css nfo markdown asciidoc cfg conf ada cs d nim c cpp hs rb lua moon fs go py sh js sql tex")
+  ret+=(=$(__join \| "au mid midi mka mpc ra axa oga spx xspf flac ogg mp3 m4a aac wav avi mov m2v ogm mp4v vob qt nuv asd rm rmvb flc fli gl m2ts divx axv anx ogv ogx mkv webm flv mp4 m4v mpg mpeg gif bmp pbm pgm ppm tga xbm xpm tif tiff svg svgz mng pcx dl xcf xwd yuv cgm emf eps cr2 ico jpg jpeg png msi exe fla iso xz zip tar 7z gz bz bz2 apk tgz lzma arj taz lzh tlz txz z dz lz tbz tbz2 tz deb rpm jar ace rar zoo cpio rz gem docx pdf odt sqlite log bak aux lof lol lot toc bbl blg tmp temp swp incomplete o class cache pyc aria2 torrent torrent.added part crdownload"))
+  local ret2=$(__join \| "${ret[@]}")
   printf "%q$" "($ret2)"
 }
 
