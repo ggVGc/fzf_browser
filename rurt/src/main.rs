97f35826019bd1e2e96af6d26b3a6c9c562bd2da
Chris West (Faux) <chris@tessl.io>
2025-01-16 18:03:25 +0000

    feat: colour more files; resolve paths

--- rurt/src/main.rs
+++ rurt/src/main.rs
@@ -1,12 +1,13 @@
-use anyhow::anyhow;
-use anyhow::Result;
+use anyhow::{anyhow, Context};
+use anyhow::{bail, Result};
 use clap::Parser;
 use skim::prelude::*;
 use std::ffi::OsString;
-use std::path::PathBuf;
+use std::fs::FileType;
+use std::path::{Path, PathBuf};
 use std::process::ExitCode;
 use std::{fs, thread};
-use tuikit::attr::Color;
+use tuikit::attr::{Attr, Color};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -17,7 +18,7 @@ struct Cli {
 
 struct FileName {
     name: OsString,
-    is_dir: bool,
+    file_type: FileType,
 }
 
 impl SkimItem for FileName {
@@ -27,58 +28,63 @@ impl SkimItem for FileName {
 
     fn display<'a>(&'a self, _context: DisplayContext<'a>) -> AnsiString<'a> {
         let s = self.name.to_string_lossy().to_string();
-        if self.is_dir {
-            let whole = (0, s.len() as u32);
-            AnsiString::new_string(s, vec![(Color::RED.into(), whole)])
+        return if self.file_type.is_dir() {
+            colour_whole(s, Color::LIGHT_BLUE)
+        } else if self.file_type.is_symlink() {
+            colour_whole(s, Color::LIGHT_CYAN)
+        } else if self.file_type.is_file() {
+            s.into()
         } else {
-            AnsiString::from(s)
-        }
+            colour_whole(s, Color::LIGHT_RED)
+        };
     }
+}
+
+fn colour_whole(s: String, attr: impl Into<Attr>) -> AnsiString<'static> {
+    let whole = (0, s.len() as u32);
+    AnsiString::new_string(s, vec![(attr.into(), whole)])
 }
 
 fn main() -> Result<ExitCode> {
     let cli = Cli::parse();
-    let mut here = PathBuf::from(cli.start_path);
+    let mut here = fs::canonicalize(cli.start_path).context("start path")?;
 
     let mut options = SkimOptions::default();
     options.no_clear = true;
     options.bind.push("left:abort".to_string());
-    options.bind.push("right:accept".to_string());
+    options.bind.push("right:abort".to_string());
 
     loop {
         let (tx, rx) = unbounded::<Arc<dyn SkimItem>>();
         options.prompt = format!("{} > ", here.to_string_lossy());
         let here_copy = here.clone();
         let throd = thread::spawn(move || -> Result<()> {
-            tx.send(Arc::new(FileName {
-                name: OsString::from(".."),
-                is_dir: true,
-            }))?;
-
             for f in fs::read_dir(here_copy)? {
                 let f = f?;
-                let is_dir = f.file_type()?.is_dir();
                 let name = f.file_name();
-                tx.send(Arc::new(FileName { name, is_dir }))?;
+                let file_type = f
+                    .file_type()
+                    .with_context(|| anyhow!("retrieving type of {:?}", &name))?;
+
+                // err: disconnected
+                if tx.send(Arc::new(FileName { name, file_type })).is_err() {
+                    break;
+                }
             }
             Ok(())
         });
 
         let output = Skim::run_with(&options, Some(rx)).ok_or_else(|| anyhow!("skim said NONE"))?;
 
-        // if output.is_abort {
-        //     return Ok(ExitCode::FAILURE);
-        // }
-
-        let mut requested_directory = false;
+        let mut requested_navigation = false;
 
         match output.final_key {
             Key::Left => {
-                here.push("..");
+                here.pop();
                 continue;
             }
             Key::Right => {
-                requested_directory = true;
+                requested_navigation = true;
             }
             _ => {
                 if output.is_abort {
@@ -99,11 +105,22 @@ fn main() -> Result<ExitCode> {
             .downcast_ref::<FileName>()
             .expect("single type");
 
-        if requested_directory && item.is_dir {
-            here.push(&item.name);
-        } else if !requested_directory {
+        if requested_navigation {
+            if let Ok(cand) = ensure_directory(here.join(&item.name)) {
+                here = cand;
+            }
+        } else {
             println!("{}", here.join(&item.name).to_string_lossy());
             return Ok(ExitCode::SUCCESS);
         }
     }
+}
+
+fn ensure_directory(p: impl AsRef<Path>) -> Result<PathBuf> {
+    let canon = fs::canonicalize(p)?;
+    if !fs::metadata(&canon)?.is_dir() {
+        bail!("resolved path is not a directory");
+    }
+
+    Ok(canon)
 }
