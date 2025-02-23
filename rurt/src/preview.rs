aa838928e3311b41f873edab960c066c8cd34aeb
Chris West (Faux) <git@goeswhere.com>
2025-02-23 21:19:15 +0000

    feat: preview file read is in-proc and fast enough

--- rurt/src/preview.rs
+++ rurt/src/preview.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -20,7 +21,16 @@ pub struct PreviewedData {
 
 pub fn run_preview(pathref: impl AsRef<Path>, preview: Arc<Mutex<PreviewedData>>) -> Result<()> {
     let path = pathref.as_ref();
-    let command = if path.is_file() { "cat" } else { "ls" };
+    if path.is_file() {
+        {
+            let mut preview = preview.lock().expect("panic");
+            preview.command = "cat".to_owned();
+        }
+        stream_some(fs::File::open(path)?, preview)?;
+        return Ok(());
+    }
+
+    let command = "ls";
     let spawn = Command::new(command)
         .args(&[path])
         .stdout(std::process::Stdio::piped())
@@ -31,10 +41,15 @@ pub fn run_preview(pathref: impl AsRef<Path>, preview: Arc<Mutex<PreviewedData>>
         let mut preview = preview.lock().expect("panic");
         preview.command = command.to_owned();
     }
-    let mut stdout = spawn.stdout.expect("piped");
+    stream_some(spawn.stdout.expect("piped"), preview)?;
+    Ok(())
+}
+
+fn stream_some(reader: impl Read, preview: Arc<Mutex<PreviewedData>>) -> Result<()> {
+    let mut reader = reader;
     let mut buf = [0u8; 1024];
     loop {
-        let bytes = stdout.read(&mut buf)?;
+        let bytes = reader.read(&mut buf)?;
         if bytes == 0 {
             break;
         }
