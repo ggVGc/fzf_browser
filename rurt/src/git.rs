538a3781913c476e534cef71ad212bee95681c43
Chris West (Faux) <git@goeswhere.com>
2025-03-20 21:27:49 +0000

    wip

--- rurt/src/git.rs
+++ rurt/src/git.rs
@@ -1,7 +1,8 @@
 use crate::cache::Cache;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use gix::revision::walk::Sorting;
 use gix::Repository;
+use log::info;
 use std::cell::RefCell;
 use std::path::{Path, PathBuf};
 
@@ -25,7 +26,7 @@ impl Git {
     pub fn resolve(&self, path: impl AsRef<Path>) -> Option<String> {
         let path = path.as_ref();
         let repo = self.repo.clone();
-        let root = self.root.clone();
+        let root = repo.work_dir()?.to_path_buf();
         let path_2 = path.to_path_buf();
         self.cache
             .borrow_mut()
@@ -42,12 +43,26 @@ fn find_file_commit(
     target: impl AsRef<Path>,
 ) -> Result<Option<String>> {
     let target = target.as_ref().strip_prefix(root)?;
+    let targets_dir = target.parent().ok_or_else(|| anyhow!("not the root!"))?;
     Ok(repo
         .rev_walk([repo.head()?.into_peeled_id()?])
         .sorting(Sorting::ByCommitTime(Default::default()))
         .all()?
         .find_map(|info| {
             let info = info.ok()?;
+            let commit_tree = repo.find_commit(info.id).ok()?.tree().ok()?;
+            let tree = if targets_dir.components().count() == 0 {
+                commit_tree
+            } else {
+                commit_tree
+                    .lookup_entry_by_path(targets_dir)
+                    .ok()??
+                    .object()
+                    .ok()?
+                    .try_into_tree()
+                    .ok()?
+            };
+            info!("tree: {:?}", tree.decode().ok());
             let here = repo
                 .rev_parse_single(format!("{}:{}", info.id, target.display()).as_str())
                 .ok()?;
